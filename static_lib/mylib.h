a0918d9e6ac14cec7af661af4ca01478b8c32a70
speculum-factorem <f4gbure@yandex.ru>
2025-12-11 17:20:52 +0300

    Enhanced static library with more complex number operations: - Added divide() function with error handling - Added argument() function to compute angle - Added conjugate() function - Added power() function for integer exponents - Updated header file with new function declarations

--- static_lib/mylib.h
+++ static_lib/mylib.h
@@ -7,11 +7,18 @@ namespace MathLib {
         double imag;
     };
 
+    // Существующие функции
     Complex add(const Complex& a, const Complex& b);
     Complex subtract(const Complex& a, const Complex& b);
     Complex multiply(const Complex& a, const Complex& b);
     double modulus(const Complex& c);
     void printComplex(const Complex& c);
+    
+    // НОВЫЕ ФУНКЦИИ для ветки static-library
+    Complex divide(const Complex& a, const Complex& b);
+    double argument(const Complex& c);  // аргумент (угол) комплексного числа
+    Complex conjugate(const Complex& c); // комплексно-сопряженное число
+    Complex power(const Complex& c, int n); // возведение в степень
 }
 
 #endif
